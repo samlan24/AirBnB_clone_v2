de79c907980bacd670cbf9eb2babbfcc944355f9
samlan24 <muchemionline@gmail.com>
2024-06-29 11:51:39 -0700

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,12 @@
---script that creates a new database, a new user
---and grants priviliges
-
+-- this script prepares a MySQL server for the project
+-- create project developement database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---creating a new database
-CREATE USER IF NOT EXISTS 'hbnb_dev'@l'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---creating a new user
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it dosen't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
---granting all privileges
 FLUSH PRIVILEGES;
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
---granting select privilege
 FLUSH PRIVILEGES;
