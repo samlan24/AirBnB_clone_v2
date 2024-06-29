331ba616d78097e0bbd54bfd8e734f68ed312da3
samlan24 <muchemionline@gmail.com>
2024-06-29 11:49:40 -0700

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,13 @@
---script that creates a new database
---creates a new user
---grants priviledges
+--script that creates a new database, a new user
+--and grants priviliges
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+--creating a new database
 CREATE USER IF NOT EXISTS 'hbnb_dev'@l'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--creating a new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+--granting all privileges
 FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+--granting select privilege
 FLUSH PRIVILEGES;
